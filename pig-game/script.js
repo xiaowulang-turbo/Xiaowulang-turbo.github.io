d922d3032b8824fcc6339a34d625c20580d55c1a
Xiaowu <93776841+xiaowulang-turbo@users.noreply.github.com>
2023-11-19 22:01:05 +0800

    Update script.js

--- pig-game/script.js
+++ pig-game/script.js
@@ -19,6 +19,7 @@ diceEl.classList.add("hidden");
 
 let currentScore = 0;
 let score = 0;
+let flag = 1;
 //currentScoreEl.textContent = 0;
 //let player = 0;
 
@@ -31,35 +32,50 @@ const playerChange = function () {
 };
 
 btnRoll.addEventListener("click", function () {
-  const dice = Number(Math.trunc(Math.random() * 6 + 1));
-  diceEl.classList.remove("hidden");
-  diceEl.src = `pig-game/dice-${dice}.png`;
-  //console.log(`dice=${dice}`);
-  //console.log(currentScoreEl.id);
-  if (dice === 1) {
-    currentScore = 0;
-    document.getElementById(`current--${player}`).textContent = 0;
-    playerChange();
-  } else {
-    currentScore += dice;
+  if (flag == 1) {
+    const dice = Number(Math.trunc(Math.random() * 6 + 1));
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+    console.log(`dice=${dice}`);
+    //console.log(currentScoreEl.id);
+    if (dice === 1) {
+      currentScore = 0;
+      document.getElementById(`current--${player}`).textContent = 0;
+      playerChange();
+    } else {
+      currentScore += dice;
+    }
+    //console.log(`currentScore=${currentScore}`);
+    document.getElementById(`current--${player}`).textContent = currentScore;
   }
-  //console.log(`currentScore=${currentScore}`);
-  document.getElementById(`current--${player}`).textContent = currentScore;
 });
 
 btnHold.addEventListener("click", function () {
-  score = Number(document.querySelector(`#score--${player}`).textContent);
-  score += Number(currentScore);
-  document.querySelector(`#score--${player}`).textContent = score;
-  currentScore = 0;
-  score = 0;
-  document.getElementById(`current--${player}`).textContent = currentScore;
-  playerChange();
+  if (flag == 1) {
+    score = Number(document.querySelector(`#score--${player}`).textContent);
+    score += Number(currentScore);
+    if (score >= 30) {
+      document
+        .querySelector(`.player--${player}`)
+        .classList.add("player--winner");
+      flag = 0;
+    } else {
+      playerChange();
+    }
+    document.querySelector(`#score--${player}`).textContent = score;
+    currentScore = 0;
+    score = 0;
+    document.getElementById(`current--${player}`).textContent = currentScore;
+  }
 
   //currentScore.textContent
 });
 
 btnNew.addEventListener("click", function () {
+  flag = 1;
+  document
+    .querySelector(`.player--${player}`)
+    .classList.remove("player--winner");
   currentScore = 0;
   score = 0;
   score0El.textContent = 0;
